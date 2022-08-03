7b3f13dcf56fcb62f2171a59fdabfc78f8af8165
VictorVidal08 <victorvidal08@hotmail.com>
2022-08-03 09:50:21 -0300

    feat: adding /talker endpoint to display talkers array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,11 +8,33 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// iniciando projeto
+// ref async function: https://nodejs.dev/learn/reading-files-with-nodejs
+
+const talkers = async () => {
+  try {
+    const data = await fs.readFile('./talker.json', { encoding: 'utf8' });
+    // console.log(data);
+    return JSON.parse(data);
+  } catch (err) {
+    // console.log(err);
+    return err;
+  }
+};
+
+talkers();
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkersResponse = await talkers();
+  // console.log(talkersResponse);
+  if (talkersResponse.length === 0) {
+    return res.status(HTTP_OK_STATUS).json([]);
+  }
+  return res.status(HTTP_OK_STATUS).json(talkersResponse);
 });
 
 app.listen(PORT, () => {
