5ebcc2244a4de2a27d9c0a9baa1901c8cefefd5a
VictorVidal08 <victorvidal08@hotmail.com>
2022-08-03 17:13:25 -0300

    feat: starting authorization validations

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const fs = require('fs/promises');
 const crypto = require('crypto');
 const emailValidation = require('./emailValidation');
 const passwordValidation = require('./passwordvalidation');
+const nameValidation = require('./nameValidation');
+const ageValidation = require('./ageValidation');
+const talkValidation = require('./talkValidation');
+const rateValidation = require('./rateValidation');
+const authValidation = require('./authorizationValidation');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,8 +18,6 @@ const PORT = '3000';
 
 // ref async function: https://nodejs.dev/learn/reading-files-with-nodejs
 
-// var dateReg = /^\d{2}[./-]\d{2}[./-]\d{4}$/ 
-
 const talkers = async () => {
   try {
     const data = await fs.readFile('./talker.json', { encoding: 'utf8' });
@@ -23,6 +26,23 @@ const talkers = async () => {
   } catch (err) {
     return err;
   }
+};
+
+const addTalker = async (req, res) => {
+  const { name, age, talk } = req.body;
+
+  const id = Date.now;
+
+  const newTalker = {
+    id,
+    name,
+    age,
+    talk,
+  };
+
+  const content = await fs.readFile('./talker.json', { encoding: 'utf8' });
+  const data = JSON.parse(content);
+  data.push(newTalker);
 };
 
 function generateToken() {
@@ -60,6 +80,17 @@ app.post('/login', emailValidation, passwordValidation, (_req, res) => {
   const token = generateToken();
 
   res.status(HTTP_OK_STATUS).json({ token });
+});
+
+app.post('/talker', 
+  authValidation, nameValidation, ageValidation, talkValidation, rateValidation, (req, res) => {
+  const { name, age, talk } = req.body;
+  const { authorization } = req.headers;
+  
+  console.log(name, age, talk.watchedAt, talk.rate);
+  console.log(typeof (age));
+  console.log(authorization);
+  res.send('olá');
 });
 
 app.listen(PORT, () => {
